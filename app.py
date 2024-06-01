e299ae96dd84dd6b8e4349a2bc95a237f3b86a63
EnriqueForero <43864186+EnriqueForero@users.noreply.github.com>
2024-06-01 09:54:10 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 import streamlit as st
-# Add custom CSS to hide the GitHub icon
-hide_github_icon = """
-#GithubIcon {
-  visibility: hidden;
-}
-"""
-st.markdown(hide_github_icon, unsafe_allow_html=True)
+st.markdown(
+    """
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK {
+        display: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
 import funciones as fn
 from snowflake_utils import sf_check_snowflake_connection, st_query_to_snowflake_and_return_dataframe
